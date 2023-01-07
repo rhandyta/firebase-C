3ea5c8c95e13634dd31e6c3ca72307c4867aab50
Rhandyta Briantama <rhandytabriantama@gmail.com>
2023-01-07 17:43:11 +0700

    delete document

--- src/component/StudentItem.js
+++ src/component/StudentItem.js
@@ -1,6 +1,11 @@
+import { deleteDoc, doc } from "firebase/firestore";
 import React from "react";
+import { db } from "../config";
 
 function StudentItem({ student, no }) {
+    const deleteHandler = async () => {
+        await deleteDoc(doc(db, "students", student.id));
+    };
     return (
         <tr key={student.id}>
             <td className="no">{no}</td>
@@ -8,7 +13,9 @@ function StudentItem({ student, no }) {
             <td>{student.address}</td>
             <td>{student.major}</td>
             <td className="action">
-                <button className="btn btn-delete">Delete</button>
+                <button className="btn btn-delete" onClick={deleteHandler}>
+                    Delete
+                </button>
                 <button className="btn btn-edit">Edit</button>
             </td>
         </tr>
