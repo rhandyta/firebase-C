95f53c8daca5f0bef204bada8d4a8ce13dff0526
Rhandyta Briantama <rhandytabriantama@gmail.com>
2023-01-08 10:17:49 +0700

    Register authentication firebase

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,76 +1,99 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Form, Formik, Field, ErrorMessage } from 'formik';
-import * as yup from 'yup';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Form, Formik, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../config";
 
 const Login = () => {
-  const initialValues = {
-    email: '',
-    password: '',
-  };
+    const initialValues = {
+        email: "",
+        password: "",
+    };
 
-  const validationSchema = yup.object({
-    email: yup.string().required().email(),
-    password: yup.string().required().min(6),
-  });
+    const validationSchema = yup.object({
+        email: yup.string().required().email(),
+        password: yup.string().required().min(6),
+    });
 
-  const onSubmit = (values) => {
-    console.log(values);
-  };
-  return (
-    <>
-      <h1>Login</h1>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={onSubmit}
-      >
-        {(props) => {
-          return (
-            <Form autoComplete="off">
-              <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <Field
-                  id="email"
-                  className="input"
-                  name="email"
-                  type="text"
-                  placeholder="Email"
-                />
-                <ErrorMessage name="email">
-                  {(err) => <span className="error-message">{err}</span>}
-                </ErrorMessage>
-              </div>
-              <div className="col">
-                <div className="form-group">
-                  <label htmlFor="password">Password</label>
-                  <Field
-                    id="password"
-                    className="input"
-                    name="password"
-                    type="password"
-                    placeholder="Password"
-                  />
-                  <ErrorMessage name="password">
-                    {(err) => <span className="error-message">{err}</span>}
-                  </ErrorMessage>
-                </div>
-              </div>
-              <button
-                className="btn btn-add"
-                disabled={!props.isValid || props.isSubmitting}
-              >
-                {props.isSubmitting ? 'Please Wait' : 'Login'}
-              </button>
-              <span className="text-sm">
-                Belum punya akun? <Link to={'/register'}>Register</Link>
-              </span>
-            </Form>
-          );
-        }}
-      </Formik>
-    </>
-  );
+    const onSubmit = async (values) => {
+        await createUserWithEmailAndPassword(
+            auth,
+            values.email,
+            values.password
+        )
+            .then((res) => {
+                const user = res.user;
+                console.log(user);
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+            });
+    };
+    return (
+        <>
+            <h1>Login</h1>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={onSubmit}
+            >
+                {(props) => {
+                    return (
+                        <Form autoComplete="off">
+                            <div className="form-group">
+                                <label htmlFor="email">Email</label>
+                                <Field
+                                    id="email"
+                                    className="input"
+                                    name="email"
+                                    type="text"
+                                    placeholder="Email"
+                                />
+                                <ErrorMessage name="email">
+                                    {(err) => (
+                                        <span className="error-message">
+                                            {err}
+                                        </span>
+                                    )}
+                                </ErrorMessage>
+                            </div>
+                            <div className="col">
+                                <div className="form-group">
+                                    <label htmlFor="password">Password</label>
+                                    <Field
+                                        id="password"
+                                        className="input"
+                                        name="password"
+                                        type="password"
+                                        placeholder="Password"
+                                    />
+                                    <ErrorMessage name="password">
+                                        {(err) => (
+                                            <span className="error-message">
+                                                {err}
+                                            </span>
+                                        )}
+                                    </ErrorMessage>
+                                </div>
+                            </div>
+                            <button
+                                className="btn btn-add"
+                                disabled={!props.isValid || props.isSubmitting}
+                            >
+                                {props.isSubmitting ? "Please Wait" : "Login"}
+                            </button>
+                            <span className="text-sm">
+                                Belum punya akun?{" "}
+                                <Link to={"/register"}>Register</Link>
+                            </span>
+                        </Form>
+                    );
+                }}
+            </Formik>
+        </>
+    );
 };
 
 export default Login;
--- src/component/Register.js
+++ src/component/Register.js
@@ -1,93 +1,129 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Form, Formik, Field, ErrorMessage } from 'formik';
-import * as yup from 'yup';
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Form, Formik, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../config";
 
 const Register = () => {
-  const initialValues = {
-    email: '',
-    password: '',
-    confirmPassword: '',
-  };
+    const navigate = useNavigate();
+    const initialValues = {
+        email: "",
+        password: "",
+        confirmPassword: "",
+    };
 
-  const validationSchema = yup.object({
-    email: yup.string().required().email(),
-    password: yup.string().required().min(6),
-    confirmPassword: yup
-      .string()
-      .oneOf([yup.ref('password'), null], 'Password must match'),
-  });
+    const validationSchema = yup.object({
+        email: yup.string().required().email(),
+        password: yup.string().required().min(6),
+        confirmPassword: yup
+            .string()
+            .oneOf([yup.ref("password"), null], "Password must match"),
+    });
 
-  const onSubmit = (values) => {
-    console.log(values);
-  };
-  return (
-    <>
-      <h1>Register</h1>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={onSubmit}
-      >
-        {(props) => {
-          return (
-            <Form autoComplete="off">
-              <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <Field
-                  id="email"
-                  className="input"
-                  name="email"
-                  type="text"
-                  placeholder="Email"
-                />
-                <ErrorMessage name="email">
-                  {(err) => <span className="error-message">{err}</span>}
-                </ErrorMessage>
-              </div>
-              <div className="col">
-                <div className="form-group">
-                  <label htmlFor="password">Password</label>
-                  <Field
-                    id="password"
-                    className="input"
-                    name="password"
-                    type="password"
-                    placeholder="Password"
-                  />
-                  <ErrorMessage name="password">
-                    {(err) => <span className="error-message">{err}</span>}
-                  </ErrorMessage>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="confirm-password">Confirm Password</label>
-                  <Field
-                    id="confirm-password"
-                    className="input"
-                    name="confirmPassword"
-                    type="password"
-                    placeholder="Confirm Password"
-                  />
-                  <ErrorMessage name="confirmPassword">
-                    {(err) => <span className="error-message">{err}</span>}
-                  </ErrorMessage>
-                </div>
-              </div>
-              <button
-                className="btn btn-add"
-                disabled={!props.isValid || props.isSubmitting}
-              >
-                {props.isSubmitting ? 'Please Wait' : 'Register'}
-              </button>
-              <span className="text-sm">
-                Sudah punya akun? <Link to={'/login'}>Login</Link>
-              </span>
-            </Form>
-          );
-        }}
-      </Formik>
-    </>
-  );
+    const onSubmit = async (values) => {
+        await createUserWithEmailAndPassword(
+            auth,
+            values.email,
+            values.password
+        )
+            .then((res) => {
+                const user = res.user;
+                alert("Login Succcessfully");
+                navigate("/login");
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                if (errorCode == "auth/email-already-in-use") {
+                    return alert("Email already exist");
+                }
+            });
+    };
+    return (
+        <>
+            <h1>Register</h1>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={onSubmit}
+            >
+                {(props) => {
+                    return (
+                        <Form autoComplete="off">
+                            <div className="form-group">
+                                <label htmlFor="email">Email</label>
+                                <Field
+                                    id="email"
+                                    className="input"
+                                    name="email"
+                                    type="text"
+                                    placeholder="Email"
+                                />
+                                <ErrorMessage name="email">
+                                    {(err) => (
+                                        <span className="error-message">
+                                            {err}
+                                        </span>
+                                    )}
+                                </ErrorMessage>
+                            </div>
+                            <div className="col">
+                                <div className="form-group">
+                                    <label htmlFor="password">Password</label>
+                                    <Field
+                                        id="password"
+                                        className="input"
+                                        name="password"
+                                        type="password"
+                                        placeholder="Password"
+                                    />
+                                    <ErrorMessage name="password">
+                                        {(err) => (
+                                            <span className="error-message">
+                                                {err}
+                                            </span>
+                                        )}
+                                    </ErrorMessage>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="confirm-password">
+                                        Confirm Password
+                                    </label>
+                                    <Field
+                                        id="confirm-password"
+                                        className="input"
+                                        name="confirmPassword"
+                                        type="password"
+                                        placeholder="Confirm Password"
+                                    />
+                                    <ErrorMessage name="confirmPassword">
+                                        {(err) => (
+                                            <span className="error-message">
+                                                {err}
+                                            </span>
+                                        )}
+                                    </ErrorMessage>
+                                </div>
+                            </div>
+                            <button
+                                type="submit"
+                                className="btn btn-add"
+                                disabled={!props.isValid || props.isSubmitting}
+                            >
+                                {props.isSubmitting
+                                    ? "Please Wait"
+                                    : "Register"}
+                            </button>
+                            <span className="text-sm">
+                                Sudah punya akun?{" "}
+                                <Link to={"/login"}>Login</Link>
+                            </span>
+                        </Form>
+                    );
+                }}
+            </Formik>
+        </>
+    );
 };
 
 export default Register;
